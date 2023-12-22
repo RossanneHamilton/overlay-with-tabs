3d42b3fdcfb2249c115ede8ce52858b94efd48c8
Lithill <rossanneh@gmail.com>
2023-12-22 15:53:00 +0000

    Add toggle overlay functionality

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
+let activeTab = null;
+// Make sure default tab is visible
+document.getElementById("defaultOpen").click();
+let chatbotOpen = false; // Track the chatbot's state
+
+// function for switching between tabs
 function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Remove the background color and change text color of all tablinks/buttons
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].style.backgroundColor = "";
-        tablinks[i].style.color = "black"; // Set text color back to white for all tabs
+        tablinks[i].style.color = "black";
     }
 
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
+    var pageElement = document.getElementById(pageName);
 
-    // Set the specific color and text color for the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-    elmnt.style.color = "black"; // Change text color to black for the clicked tab
+    if (pageElement === activeTab) {
+        activeTab.style.display = "none";
+        activeTab = null;
+        off(); // Call off() to hide elements
+    } else {
+        elmnt.style.backgroundColor = color;
+        elmnt.style.color = "black";
+        pageElement.style.display = "block";
+        activeTab = pageElement;
+    }
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+// Chatbot overlay function
+function toggleChatbot() {
+    const askAbout = document.getElementById("askAbout");
+    const askAboutText = document.getElementById("askAboutText");
+    const tabs = document.getElementById("tabs");
+
+    if (chatbotOpen) {
+        askAbout.style.display = "none";
+        askAboutText.style.display = "none";
+        tabs.style.display = "none";
+    } else {
+        askAbout.style.display = "block";
+        askAboutText.style.display = "block";
+        tabs.style.display = "block";
+    }
+
+    chatbotOpen = !chatbotOpen; // Toggle chatbot's state
+}
